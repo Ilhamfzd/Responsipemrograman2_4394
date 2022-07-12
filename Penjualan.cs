496d1af76c9fd942b4df9233de5b620db8e1bcd3
Ilhamfzd <101569510+Ilhamfzd@users.noreply.github.com>
2022-07-12 11:44:58 +0700

    Add files via upload

---
+++ Penjualan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Responsipemrograman_4394
+{
+    public class Penjualan
+    {
+        public string Nota { get; set; }
+        public string Tanggal { get; set; }
+        public string Customer { get; set; }
+        public string Jenis { get; set; }
+        public int Total { get; set; }
+    }
+}
